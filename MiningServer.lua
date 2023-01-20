d60474532f9e41c5f22d2645c05ebf072b146828
Lars <lars@pfrenger.com>
2023-01-20 18:52:55 +0100

    pain

--- MiningServer.lua
+++ MiningServer.lua
@@ -7,6 +7,8 @@ local MiningServer = {
     Clients = {},
 }
 
+local Monitor = nil
+
 function Init() 
     -- Setup Hostname
     assert(#tArgs == 1, "Invalid Syntax: Please provide a name for the host")
@@ -15,22 +17,21 @@ function Init()
 
     -- Setup Packet Handlers
     MSTP.Packets["Register"] = Packet_Register
-
+    
     --Setup Monitor
-    local monitor = peripheral.find("monitor")
-    term.redirect(monitor)
+    Monitor = peripheral.find("monitor")
+    term.redirect(Monitor)
     term.clear()
-    term.setCursorPos(1,1)
-
+    
     -- Setup RedNet
     peripheral.find("modem", rednet.open)
     assert(rednet.isOpen(), "Could not find a wireless modem")
-
+    
     rednet.host(MSTP.PROTOCOL_NAME, MiningServer.Name)
     
     print("Hostname: " .. tArgs[1])
     print("Hosting " .. MSTP.PROTOCOL_NAME .. " Protocol")
-
+    
     while true do
         Update()
     end
@@ -41,7 +42,19 @@ function Packet_Register(id, name)
     MSTP.SendPacket(id, "Register", "The cake is a lie")
 end
 
+function Draw()
+
+    local width, height = Monitor.getSize()
+    term.clear()
+    term.setCursorPos(1,1)
+    paintutils.drawBox(2, 3, 30, 7, colors.red)
+
+
+end
+
 function Update() 
+    Draw()
+    
     local event, param1, param2, param3 = os.pullEvent()
 
     if event == "rednet_message" then
